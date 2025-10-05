b73b4a4f28abda5d39b9b0944ead53a6e2f89170
Thiago Trevizam Dorini <thiagotdorini@gmail.com>
2025-10-05 05:40:10 -0300

    up

--- teros/functions/thermodynamics/__init__.py
+++ teros/functions/thermodynamics/__init__.py
@@ -7,10 +7,10 @@ for binary and ternary oxide systems.
 
 from teros.functions.thermodynamics.formation import calculate_formation_enthalpy
 from teros.functions.thermodynamics.binary import calculate_surface_energy_binary
-from teros.functions.thermodynamics.ternary import calculate_surface_energy_ternar
+from teros.functions.thermodynamics.ternary import calculate_surface_energy_ternary
 
 __all__ = [
     'calculate_formation_enthalpy',
     'calculate_surface_energy_binary',
-    'calculate_surface_energy_ternar',
+    'calculate_surface_energy_ternary',
 ]
