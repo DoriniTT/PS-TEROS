595e502bd636b302494227c3f5c509ce7d5d82aa
Thiago Trevizam Dorini <thiagotdorini@gmail.com>
2025-10-13 09:11:33 -0300

    feat(aimd): add aimd_builder with get_aimd_defaults

--- teros/core/builders/__init__.py
+++ teros/core/builders/__init__.py
@@ -7,9 +7,11 @@ Contains default parameter builders for various material systems.
 from .default_ag2o_builders import get_ag2o_defaults
 from .default_ag3po4_builders import get_ag3po4_defaults
 from .electronic_properties_builder import get_electronic_properties_defaults
+from .aimd_builder import get_aimd_defaults
 
 __all__ = [
     'get_ag2o_defaults',
     'get_ag3po4_defaults',
     'get_electronic_properties_defaults',
+    'get_aimd_defaults',
 ]
