e9e42f733810dc69cc535a60c42e41a0e37d95d1
Thiago Trevizam Dorini <thiagotdorini@gmail.com>
2025-10-21 08:28:23 -0300

    feat: move surface_modes.py to core module

--- teros/core/surface_hydroxylation/__init__.py
+++ teros/core/surface_hydroxylation/__init__.py
@@ -1,5 +1,6 @@
 """Surface hydroxylation module for PS-TEROS."""
 
 from .workgraph import SurfaceHydroxylationWorkGraph
+from .surface_modes import SurfaceModifier
 
-__all__ = ['SurfaceHydroxylationWorkGraph']
+__all__ = ['SurfaceHydroxylationWorkGraph', 'SurfaceModifier']
